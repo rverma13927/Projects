a8ad96a0db14542590b867b7b5ef4f334e6944bb
deepak <rverma13927@gmail.com>
2025-03-18 22:06:54 +0530

    FLashcard: Added swagger

---
+++ Flashcard/flashcard/src/main/java/com/deepak/flashcard/model/Topic.java
@@ -0,0 +1,25 @@
+package com.deepak.flashcard.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "topics")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    // Getters & Setters
+}
